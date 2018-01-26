8194886661190ad10374cc108d6d907b0527757a
Vincent Perrin <vincent.perrin@fr.ibm.com>
2018-01-26 10:56:23 +0100

    Using B64 encoding
    You have to provide username & password of WDS as input parameters instead of B64 authorization.

---
+++ WDS_V2.js
@@ -0,0 +1,60 @@
+ /**
+  *
+  * main() will be invoked when you Run This Action
+  *
+  * @param environment_id => Watson Discovery Environment ID
+  * @param collection_id => Watson Discovery collection ID
+  * @param input => User Text input
+  * @param url => Watson Gateway URL
+  * @param username => WDS username
+  * @param password => WDS password
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+  
+  function main(params) {
+    var request = require('request');
+    const btoa = require("btoa");
+    var environment_id = params.environment_id;
+    var collection_id = params.collection_id ;
+    var input = params.input;
+    var url = params.url || 'https://gateway.watsonplatform.net';
+    var wds_username = params.username;
+    var wds_password = params.password;
+
+    var authorization = "Basic " + btoa((wds_username + ":" + wds_password));
+    
+    var qs=
+     { version: '2017-09-01',
+       count: '1',
+       passages: 'true',
+       natural_language_query: input };
+    
+    console.log("QS :" + JSON.stringify(qs,null,2));
+    
+    var promise = new Promise(function(resolve, reject) {request(
+    { method: 'GET',
+    url: url + '/discovery/api/v1/environments/'+environment_id+'/collections/'+collection_id+'/query',
+    qs,
+    headers: 
+     { 
+       authorization: authorization }
+     },
+       function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var j = JSON.parse(body);
+                resolve(j);
+            } else {
+                console.log('body:', body);
+                reject({
+                    error: error,
+                    response: response,
+                    body: body
+                });
+            }
+        });
+    });
+  
+    return promise
+  }
